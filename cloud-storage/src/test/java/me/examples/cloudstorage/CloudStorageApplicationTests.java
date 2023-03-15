0bc58fa13d857dd34a599d4cfb8993cd2bd7996c
김형기 <khk211113@naver.com>
2023-03-15 22:44:59 +0900

    test(main): 포트 설정 겸 웹훅 테스트

--- cloud-storage/src/test/java/me/examples/cloudstorage/CloudStorageApplicationTests.java
+++ cloud-storage/src/test/java/me/examples/cloudstorage/CloudStorageApplicationTests.java
@@ -2,12 +2,16 @@ package me.examples.cloudstorage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import scala.Array;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void contextLoads() {
