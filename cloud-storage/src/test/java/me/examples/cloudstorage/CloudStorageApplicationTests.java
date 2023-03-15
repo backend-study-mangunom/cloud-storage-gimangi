6c1675157e025ca12b33d01a1d2de1e89a63e4f8
김형기 <khk211113@naver.com>
2023-03-15 22:32:24 +0900

    test(main): 포트 중복현상 수정

--- cloud-storage/src/test/java/me/examples/cloudstorage/CloudStorageApplicationTests.java
+++ cloud-storage/src/test/java/me/examples/cloudstorage/CloudStorageApplicationTests.java
@@ -6,7 +6,7 @@ import scala.Array;
 
 import java.util.List;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
     @Test
