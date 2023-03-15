b6b26c092e13f1700c7101c33ea15a79d468971d
김형기 <khk211113@naver.com>
2023-03-15 15:26:23 +0900

    init project

---
+++ cloud-storage/src/test/java/me/examples/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,17 @@
+package me.examples.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import scala.Array;
+
+import java.util.List;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+        CloudStorageApplication.main(new String[] {});
+    }
+
+}
